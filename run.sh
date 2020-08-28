7eafcc3c3395678455cc0cabc2e26ed75deb6380
Z-H-Huang <25124355+Z-M-Huang@users.noreply.github.com>
2020-08-27 20:56:07 -0400

    Wait. I didn't install...

--- run.sh
+++ run.sh
@@ -3,5 +3,9 @@
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
     -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/7.2/apache2/php.ini
 
+echo "=> Installing playSMS ..."
+/install.sh
+echo "=> Done!"
+
 echo "=> Exec supervisord"
 exec supervisord -n
